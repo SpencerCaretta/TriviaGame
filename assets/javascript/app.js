dc244c1923e5766bee5d3fa3853120129cd4dec7
Spencer Caretta <spencer.caretta@gmail.com>
2018-10-18 15:40:39 -0500

    wrote out majority of html and some css

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// variable declaration - need array of questions and the answers to them
+// on click function for each button
+//timed function for timer
+/* correct answer function displaying a congratulations,
+ then display next question after a few seconds */
+/* run out of time function, tells player times up, display correct answer,
+ then move to next question after a few seconds */
+/* if they get the question wrong, display correct answer,
+then move to the next question after a few seconds */
+/*After the array of questions is run through, show a final screen
+Show number of correct answers, number of incorrect answers */
+//button function to restart the game for user to play again
