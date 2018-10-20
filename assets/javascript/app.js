649d46f52f22a950100dde67c38f11a80514b3da
Spencer Caretta <spencer.caretta@gmail.com>
2018-10-20 18:02:22 -0500

    some html and js changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
     $('#correctGif').hide();
     $('#incorrectGif').hide();
+    $('#timesUp').hide();
     $('#question').html("<p>" + question1.question + "</p>");
     $('#button1').html("<p>" + question1.answer1 + "</p>");
     $('#button2').html("<p>" + question1.answer2 + "</p>");
@@ -11,98 +12,95 @@ $(document).ready(function() {
     myTimer();
 
 })
-
-
 // variable declaration - need array of questions and the answers to them
-var question1 = {
+
+var questions = [question1 = {
     question: "What is the first Pokemon in the Pokedex?",
     answer1: "Pikachu",
     answer2: "Mew",
     answer3: "Bulbasaur",
     answer4: "Squirtle",
     correctAnswer: "Bulbasaur",
-}
-var question2 = {
+},
+question2 = {
     question: "How many Pokemon are in the Kanto Region?",
     answer1: 96,
     answer2: 124,
     answer3: 185,
     answer4: 151,
     correctAnswer: 151,
-}
-var question3 = {
+},
+question3 = {
     question: "What is the name of the main character (protagonist) in the Pokemon TV Show?",
     answer1: "Brock",
     answer2: "Ash",
     answer3: "Youngster Joey",
     answer4: "Gary",
     correctAnswer: "Ash",
-}
-var question4 = {
+},
+question4 = {
     question: "How many Pokemon generations of games are there currently in Pokemon? (2018)",
     answer1: 7,
     answer2: 4,
     answer3: 1,
     answer4: 6,
     correctAnswer: 7,
-}
-var question5 = {
+},
+question5 = {
     question: "How many gym badges does one need to enter the elite four in most Pokemon games?",
     answer1: 4,
     answer2: 6,
     answer3: 8,
     answer4: 10,
     correctAnswer: 8,
-}
-var question6 = {
+},
+question6 = {
     question: "What are the off-colored Pokemon that you can find called?",
     answer1: "Glitches",
     answer2: "Mutated Pokemon",
     answer3: "Special Pokemon",
     answer4: "Shiny Pokemon",
     correctAnswer: "Shiny Pokemon",
-}
-var question7 = {
+},
+question7 = {
     question: "What was the first Pokemon created?",
     answer1: "Bulbasaur",
     answer2: "Pikachu",
     answer3: "Rhydon",
     answer4: "Tangela",
     
-}
-var question8 = {
+},
+question8 = {
     question: "What is the most number of badges in a single Pokemon game? (2018)",
     answer1: 4,
     answer2: 8,
     answer3: 16,
     answer4: 24,
     correctAnswer: 16,
-}
-var question9 = {
+},
+question9 = {
     question: "How many pokemon can Eevee evolve into? (2018)",
     answer1: 3,
     answer2: 5,
     answer3: 7,
     answer4: 8,
     correctAnswer: 8,
-}
-var question10 = {
+},
+question10 = {
     question: "Who is one of the creators of Pokemon?",
     answer1: "Ken Sugimori",
     answer2: "Masahiro Sakurai",
     answer3: "Satoru Iwata",
     answer4: "Reggie Fils-Aimé",
     correctAnswer: "Ken Sugimori",
-}
-
-var questions = [question1, question2, question3, question4, question5, question6, question7, question8, question9, question10]
+},];
 
 //function used to wait 5 seconds after a question is answered, then move on to the next question 
 function wait() {
     setTimeout(
         function() {
             for (i=0; i<questions.length; i++) {
-                i++
+                i++;
                 $('#question').html(questions.question);
                 $('#button1').html(questions.answer1);
                 $('#button2').html(questions.answer2);
@@ -120,13 +118,17 @@ function run() {
 }
 /* run out of time function, tells player times up, display correct answer,
  then move to next question after a few seconds */
-function myTimer() {
+function myTimer() {  
     timeLeft--;
     $('#timeLeft').html("<p>" + timeLeft + " seconds </p>"); 
     if (timeLeft === 0) {
         alert("Times up!");
-        $('#correctAnswer').html("<p> Correct Answer: " + questions[i].correctAnswer + "</p>");
+        for (i=0; i<questions.length; i++) {
+            $('#correctAnswer').html("<p> Correct Answer: " + questions[i].correctAnswer + "</p>");
+        } 
+        $('#timesUp').show();
         wait();
+        
     }
     
 }
@@ -144,6 +146,7 @@ $('#button1').click(function() {
         $('#button3').hide();
         $('#button4').hide();
         $('#correctGif').show();
+        correct++;
         wait();
     } else {
         $('#button1').hide();
@@ -152,6 +155,7 @@ $('#button1').click(function() {
         $('#button4').hide();
         $('#correctAnswer').append(correctAnswer);
         $('#incorrectGif').show();
+        incorrect++;
         wait();
     }
 })
@@ -162,6 +166,7 @@ $('#button2').click(function() {
         $('#button3').hide();
         $('#button4').hide();
         $('#correctGif').show();
+        correct++;
         wait();
     } else {
         $('#button1').hide();
@@ -170,6 +175,7 @@ $('#button2').click(function() {
         $('#button4').hide();
         $('#correctAnswer').append(correctAnswer);
         $('#incorrectGif').show();
+        incorrect++;
         wait();
     }
     
@@ -181,6 +187,7 @@ $('#button3').click(function() {
         $('#button3').hide();
         $('#button4').hide();
         $('#correctGif').show();
+        correct++;
         wait();
     } else {
         $('#button1').hide();
@@ -189,6 +196,7 @@ $('#button3').click(function() {
         $('#button4').hide();
         $('#correctAnswer').append(correctAnswer);
         $('#incorrectGif').show();
+        incorrect++;
         wait();
     }
     
@@ -200,6 +208,7 @@ $('#button4').click(function() {
         $('#button3').hide();
         $('#button4').hide();
         $('#correctGif').show();
+        correct++;
         wait();
     } else {
         $('#button1').hide();
@@ -208,6 +217,7 @@ $('#button4').click(function() {
         $('#button4').hide();
         $('#correctAnswer').append(correctAnswer);
         $('#incorrectGif').show();
+        incorrect++;
         wait();
     }
     
@@ -215,4 +225,6 @@ $('#button4').click(function() {
 
 /*After the array of questions is run through, show a final screen
 Show number of correct answers, number of incorrect answers */
+var correct = 0;
+var incorrect = 0;
 //button function to restart the game for user to play again
